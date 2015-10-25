c75c49903bbce7a901f2069dd2ccab85b50776f2
Martin Cohen <martin.balazik@gmail.com>
2015-10-25 06:21:14 +0100

    Finished the build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,14 @@
-mkdir %~dp0build
-@pushd %~dp0build
-@rem Compiler options: https://msdn.microsoft.com/en-us/library/fwkeyyhe.aspx
-@rem Linker options:   https://msdn.microsoft.com/en-US/library/y0zzbyt4.aspx
-@set CompilerOptions=/MTd /Gm- /GR- /Od /Oi /EHa- /FC /Z7 -fp:fast  /DHALE_STU /D_DEBUG
-@set LinkerOptions=/subsystem:windows,6 /incremental:no /opt:ref
+@echo off
+
+if not exist %~dp0build mkdir %~dp0build
+pushd %~dp0build
+
+rem Compiler options: https://msdn.microsoft.com/en-us/library/fwkeyyhe.aspx
+rem Linker options:   https://msdn.microsoft.com/en-US/library/y0zzbyt4.aspx
+
+set CompilerOptions=/MTd /Gm- /GR- /Od /Oi /EHa- /FC /Z7 -fp:fast /D_HAS_EXCEPTIONS=0 /DHALE_STU /D_DEBUG
+set LinkerOptions=/subsystem:windows,6 /incremental:no /opt:ref
+
 cl /nologo %CompilerOptions% ../src/hale/stu.cpp /link %LinkerOptions% /OUT:hale.exe
-@popd
+
+popd
